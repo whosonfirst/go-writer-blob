6fd5115e0393464ede04f53a5159476f74f44908
thisisaaronland <aaron@localhost>
2021-09-09 13:07:37 -0700

    update to whosonfirst/go-writer v0.6.0; update vendor deps; inline tests

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/whosonfirst/go-writer-blob
 go 1.16
 
 require (
-	github.com/whosonfirst/go-writer v0.5.0
+	github.com/whosonfirst/go-writer v0.6.0
 	gocloud.dev v0.24.0
 )
---
+++ writer_test.go
@@ -0,0 +1,82 @@
+package writer
+
+import (
+	_ "gocloud.dev/blob/fileblob"
+)
+
+import (
+	"context"
+	"fmt"
+	"github.com/whosonfirst/go-writer"
+	// _ "github.com/whosonfirst/go-writer-blob"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestFileWriter(t *testing.T) {
+
+	ctx := context.Background()
+
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tmpdir := os.TempDir()
+	data_root := filepath.Join(tmpdir, "data")
+
+	_, err = os.Stat(data_root)
+
+	if err != nil {
+
+		err := os.MkdirAll(data_root, 0755)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	target_root := fmt.Sprintf("file://%s", data_root)
+
+	source_root := filepath.Join(cwd, "fixtures")
+	feature_path := filepath.Join(source_root, "101736545.geojson")
+
+	target_path := "101/736/545/101736545.geojson"
+
+	wr, err := writer.NewWriter(ctx, target_root)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	feature_fh, err := os.Open(feature_path)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer feature_fh.Close()
+
+	_, err = wr.Write(ctx, target_path, feature_fh)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	test_path := filepath.Join(data_root, target_path)
+
+	_, err = os.Stat(test_path)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = os.RemoveAll(data_root)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+}
